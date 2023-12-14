b371006aafe03b2140515ca6618c27cb1da850e5
Travis PENG <houdou.pt@gmail.com>
2023-12-14 15:20:35 +0800

    :bug: Fix recursive

--- index.js
+++ index.js
@@ -13,19 +13,27 @@ function scanDirectory({directory, pattern, includes, excludes}) {
         const filePath = path.join(directory, file);
         const stat = fs.statSync(filePath);
         if (stat.isDirectory()) {
-            scanDirectory(filePath);
-        } else if (stat.isFile() && includes.test(file) && !excludes.test(file)) {
-            const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
-            lines.forEach((line, index) => {
-                const match = line.match(pattern);
-                if (match) {
-                    matches.push({
-                        filePath,
-                        line,
-                        matches: matches.splice(1, 3)
-                    });
-                }
+            const inner_matches = scanDirectory({
+                directory: filePath,
+                pattern,
+                includes,
+                excludes
             });
+            inner_matches.forEach(match => matches.push(match));
+        } else if (stat.isFile()) {
+            if (includes && includes.test(file) && (!excludes || excludes && !excludes.test(file))) {
+                const lines = fs.readFileSync(filePath, 'utf-8').split('\n');
+                lines.forEach((line, index) => {
+                    const match = line.match(pattern);
+                    if (match) {
+                        matches.push({
+                            filePath,
+                            line,
+                            matches: matches.splice(1, 3)
+                        });
+                    }
+                });
+            }
         }
     }
 
@@ -35,15 +43,16 @@ function scanDirectory({directory, pattern, includes, excludes}) {
 try {
     const directory = core.getInput('directory');
     const pattern = core.getInput('pattern');
-    const includes = core.getInput('includes');
-    const excludes = core.getInput('excludes');
-
-    const INCLUDE_PATTERN = new RegExp(includes);
-    const EXCLUDE_PATTERN = new RegExp(excludes);
     const REGEX_PATTERN = new RegExp(pattern);
 
+    const includes = core.getInput('includes');
+    const INCLUDE_PATTERN = includes && new RegExp(includes);
+
+    const excludes = core.getInput('excludes');
+    const EXCLUDE_PATTERN = excludes && new RegExp(excludes);
+
     const matches = scanDirectory({
-        directory: path.resolve(directory),
+        directory,
         pattern: REGEX_PATTERN,
         includes: INCLUDE_PATTERN,
         excludes: EXCLUDE_PATTERN
